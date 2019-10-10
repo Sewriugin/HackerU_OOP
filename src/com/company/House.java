f694d445eaa82a26ceec6e7b696846186535ad20
Admin <alex.star-2011@yandex.ru>
2019-10-11 00:17:37 +0400

    1 commit

---
+++ src/com/company/House.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class House {
+    public static void main(String[] args) {
+        String color;
+        int height;
+
+        publik String getColor();
+
+    }
+    public
+
+
+
+
+
+}
