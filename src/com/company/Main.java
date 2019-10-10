eb1827f0f2865e8c153d1102998f8baf83b25e47
Admin <alex.star-2011@yandex.ru>
2019-10-10 20:11:12 +0400

    1 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import javax.print.attribute.standard.PresentationDirection;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Cat catMyua = new Cat("red");
-        Cat catNomyua = new Cat ("red", "Basyk");
+        Cat catNoMyua = new Cat ("red", "Basyk");
+        Cat catVeryMyua = new Cat("red", "Basyk", 6);
+        Cat emtyCat = new Cat();
+
         System.out.println("");
 
     }
