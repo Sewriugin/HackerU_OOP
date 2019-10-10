12f6bd0e76f1d543993af91399dff25e4b46fe39
Admin <alex.star-2011@yandex.ru>
2019-10-10 19:37:45 +0400

    1 commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Cat catMyua = new Cat("red");
+        Cat catNomyua = new Cat ("red", "Basyk");
+        System.out.println("");
+
+    }
+}
