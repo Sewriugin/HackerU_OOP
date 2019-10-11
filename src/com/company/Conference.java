dfa86fb03fa10e5d2b74f8d461c6016e5d6b646f
Admin <alex.star-2011@yandex.ru>
2019-10-11 18:59:44 +0400

    1 commit

---
+++ src/com/company/Conference.java
@@ -0,0 +1,32 @@
+/*package com.company;
+
+public class Conference {
+    String topic;
+    static int PERIOD;
+    final static String IT_CONFERENCE_TOPIC = "web programming";
+    static Conference CONS_CONFERENCE = new Conference();
+
+    public Conference() {
+    }
+
+    public Conference(String topic) {
+        this.topic = topic;
+    }
+    public String getTopic() {
+        return topic;
+    }
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+    void showTopic() {System.out.println(this.topic);}
+    static void showPeriod() {System.out.println(PERIOD);}
+    static Conference IT_CONFERENCE(){
+        //return new Conference();
+        return CONS_CONFERENCE;
+    }
+
+   // public static void mai
+    //System.out.pintln();
+}
+
+ */
