c6960766cdf006613398ce1f03ef6a08b3db19b5
Admin <alex.star-2011@yandex.ru>
2019-10-13 10:22:52 +0400

    1 Commit

---
+++ src/com/company/Airplane.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Airplane {
+    int seatingCapacity;
+    int flightRange;
+    String company;
+
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public int getFlightRange() {
+        return flightRange;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setSeatingCapacity(int seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public void setFlightRange(int flightRange) {
+        this.flightRange = flightRange;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
