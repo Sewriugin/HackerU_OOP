1da071c8d87ac8f37c3a4a7d8cc1b35d83d086df
Admin <alex.star-2011@yandex.ru>
2019-10-22 23:23:08 +0400

    1 Commit

---
+++ src/com/company/Math.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Math {
+    public static Integer convertToInteger(String value) {
+        return null;
+    }
+
+    public static String compareInteger(Integer value1, Integer value2) {
+        return null;
+    }
+
+
+
+}
+
+    //public static Integer convertTolnterger(String value) {return Integer.parseInt(value);}
+/*
+    public static String compareInteger(Integer value1, Integer value2) {int result = Integer.compare(value1,value2);}
+    String compareResultString = " ";
+        switch (result) {
+        case 1:
+            compareResultString = " Больше ";
+            break;
+        case -1:
+            compareResultString = " Меньше ";
+            break;
+        case 0:
+            compareResultString = " Равно ";
+            break;
+    }
+        return compareResultString;
+
+    public static Integer pow(Integer value, int power){
+        if (power == 0) {
+            value = 1;
+        } else if (power > 0) {
+            value = value * pow(value, -1);
+        }
+        return value;
+    }
+
+}
+*/
