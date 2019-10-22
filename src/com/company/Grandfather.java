3016fcb3cf12eb0ad2f072f0cf9cdd3e08a59951
Admin <alex.star-2011@yandex.ru>
2019-10-22 23:21:06 +0400

    1 Commit

---
+++ src/com/company/Grandfather.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Grandfather {
+
+    public String livingPlace = "livingPlace";
+
+    public void knowsWholsLenin() {
+        System.out.println(" knowsWholsLenin ");
+        }
+}
